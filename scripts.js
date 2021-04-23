e433f435223ebb97c7a6dcdfaa5d8a6520ad909b
bbaldaia <82227480+bbaldaia@users.noreply.github.com>
2021-04-23 11:49:43 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ alert("Você terá três tentativas para acertar o número secreto.")
 while (tentativas > 0) {
   var chute = parseInt(prompt("Digite um número entre 0 e 10."))
   if (numeroSecreto == chute) {
-    document.write("<h2> Acertou! </h2>")
+    alert("Acertou!")
     break
   } else if (chute > numeroSecreto) {
     alert("O número secreto é menor.")
